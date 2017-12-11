4b2722f76a2a5311ea8c4fb4414378af2e4a447d
Tech-A66 <tech-a66@Tech-A66s-MacBook-Air.local>
2017-12-11 13:50:44 +0000

    new mammal added dog

---
+++ livingOrganism/animal_types/mammals.rb
@@ -0,0 +1,49 @@
+#this is a module
+module Mammals
+
+  def self.common_mammal_traits
+    p 'Mammals are warm blooded and have common vertebrae'
+  end
+
+
+  class CommonTraits
+    def feed_young
+      p 'all mammals feed their young with milk'
+    end
+  end
+
+  class Biped < CommonTraits
+    def legs
+      p 'I have 2 legs'
+    end
+  end
+
+  class Quadruped < CommonTraits
+    def legs
+      p 'I have 4 legs cos Im a cat'
+    end
+  end
+
+  class UnusualTraits
+    def self.exceptions
+      p 'the platypus is a mammal but it has a bill and lays eggs, take that science'
+    end
+  end
+
+  class People < CommonTraits
+    def self.people
+      p 'People are cleverer than any other animal on earth'
+    end
+  end
+
+end
+
+
+# Mammals::UnusualTraits.exceptions
+# Mammals::People.people
+# Mammals::CommonTraits.feed_young
+geoff = Mammals::Biped.new
+cat = Mammals::Quadruped.new
+geoff.legs
+cat.legs
+cat.feed_young
